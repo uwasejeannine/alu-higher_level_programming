09d9d1cb22ffb5f363a819db5238369d6775e6b7
jeannineuwase <j.uwase1@alustudent.com>
2023-02-09 02:46:35 -0800

    add some changes on 5th file

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
 for i in range(100):
-<<<<<<< HEAD
-=======
     
->>>>>>> f0bdaa17d7e743720d1f3d81db447b39cfd06d19
     print("{:02d}".format(i), end=", " if i < 99 else "\n")
