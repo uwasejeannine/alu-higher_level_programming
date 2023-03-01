42cd56743742ed7488e8bee203d007ebad0351a3
jeannineuwase <j.uwase1@alustudent.com>
2023-03-01 05:06:42 -0800

    changing numbers

--- python-exceptions/5-raise_exception.py
+++ python-exceptions/5-raise_exception.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def raise_exception():
-    x = "123"
-    y = 456
+    x = "27"
+    y = 789
     if not isinstance(x, int) or not isinstance(y, int):
         raise TypeError("x and y must be integers")
