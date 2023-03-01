a5e76775aa531bf1f99dbc6bd928c86fc96c1115
jeannineuwase <j.uwase1@alustudent.com>
2023-03-01 05:18:06 -0800

    make excutable files

--- python-exceptions/100-safe_print_integer_err.py
+++ python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,8 @@
-#!/usr/bin/python3
-import sys
-
 def safe_print_integer_err(value):
+    import sys
     try:
-        integer_value = int(value)
-        print("{:d}".format(integer_value))
+        print("{:d}".format(value))
         return True
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+    except Exception as i:
+        sys.stderr.write("Exception: {}\n".format(i))
         return False
