7b89f2a83eb10ab80a20b0631b8de2aa4b26de30
jeannineuwase <j.uwase1@alustudent.com>
2023-03-14 06:52:16 -0700

    removing unnessesary code

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -6,20 +6,5 @@ class Square:
 
     Attributes:
         size (int): The length of each side of the square.
-    """
-
-    def __init__(self, size=0):
-        """Creates a new Square with a given side length.
-
-        Args:
-            size (int): The length of each side of the square.
-        """
-        self.size =Square
-
-    def area(self):
-        """Returns the area of the square.
-
-        Returns:
-            int: The area of the square (size squared).
-        """
-        return self.size ** 2
+"""
+    pass 
