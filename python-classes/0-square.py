ae332eb714d09906591f90b7833a6f474571d9ec
jeannineuwase <j.uwase1@alustudent.com>
2023-03-14 06:30:15 -0700

    add comment

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 Class Square:
+    """A square with a side length.
+
+    Attributes:
+    name (str): The length of each side of the square.
+    """
     pass
  def __init__(self,name="Jeannine"):
+     """Creates a new Square with a given side length.
+
+        Args:
+            name (str): The length of each side of the square.
+        """
     self.name=name
