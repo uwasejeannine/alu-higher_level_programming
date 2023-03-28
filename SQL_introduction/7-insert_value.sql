37ca5313cf937e72de0b5db62d27a2fad33a7abd
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 02:26:28 -0700

    add

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 -- Write a script that inserts a new row in the table first_table
 -- The database name will be passed as an argument of the mysql command
-INSERT INTO first_table (id, name) VALUES (89, 'First School');
+INSERT INTO first_table (id, name) VALUES (89, 'Holberton School');
 
