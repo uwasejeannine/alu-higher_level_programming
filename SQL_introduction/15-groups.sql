f5a9fe7ab2c2d832ffff14dc334914b11a09627b
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:57:47 -0700

    number by score

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- a script that lists the number of records with the same score in the table second_table
+-- The result should display the score and nbr of labed nbr
+SELECT score, COUNT(*) AS number FROM  second_table
+GROUP BY score
+ORDER BY score DESC;
