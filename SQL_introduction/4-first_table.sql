7ccafb84608d29701382e64b41d31b52ca7f6dd9
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 01:57:49 -0700

    create a table in database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+--a creapt that create a table called first table
+--The database name will be passed as an argument of the mysql
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
