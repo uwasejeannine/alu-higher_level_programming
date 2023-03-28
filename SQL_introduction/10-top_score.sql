2f57f0a8e9a13e13b457ae73cf4a342ec8ae3ff6
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:30:27 -0700

    add

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,4 @@
+-- A script that lists all records of the table second_table of the database
+-- The database name will be passed as an argument of the mysql command
+SELECT score, name FROM second_table
+ORDER BY score DESC;
