800be65345868956f31f832b8dbda3211135e985
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:54:53 -0700

    computes the score average

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- a script that computes the score average of all records in the table second_table
+-- The database name will be passed as an argument of the mysql command
+SELECT AVG(score) AS average FROM second_table;
