b7b47b72a906cafba165f86f2af1396b0a62ff0f
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 02:06:09 -0700

    add some comments

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
---a creapt that create a table called first table
---The database name will be passed as an argument of the mysql
+-- A script that creates a table called first_table in the current database in your MySQL server
+-- Query to create a table
 CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256));
