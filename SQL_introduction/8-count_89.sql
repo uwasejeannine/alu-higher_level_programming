3d6b461e16b6e2ceb0598f20e5fa5a9a580ba801
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 02:31:49 -0700

    Display the number of records

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- A script that displays the number of records with ad and name
+-- The database name will be passed as an argument of the mysql command
+SELECT COUNT(*) as id FROM first_table WHERE id=89;
+
