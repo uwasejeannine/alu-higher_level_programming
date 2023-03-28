333bb19c2df045680b37a4c70b6522693d3ba4a1
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:14:50 -0700

    add some space

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,9 +4,9 @@ CREATE TABLE IF NOT EXISTS second_table(
 	id INT,
 	name VARCHAR(256),
 	score INT);
-INSERT INTO second_table(id,name,score)VALUES(1,"Jeannine", 99);
-INSERT INTO second_table(id,name,score)VALUES(2,"Challes", 90);
-INSERT INTO second_table(id,name,score)VALUES(3,"Fille", 79);
-INSERT INTO second_table(id,name,score)VALUES(4,"Uwase", 77);
-INSERT INTO second_table(id,name,score)VALUES(5,"Gentille", 75);
-INSERT INTO second_table(id,name,score)VALUES(6,"Uwimbazi", 69);
+INSERT INTO second_table(id, name, score) VALUES (1, "Jeannine", 99);
+INSERT INTO second_table(id, name, score) VALUES (2, "Challes", 90);
+INSERT INTO second_table(id, name, score) VALUES (3, "Fille", 79);
+INSERT INTO second_table(id, name, score) VALUES (4, "Uwase", 77);
+INSERT INTO second_table(id, name, score) VALUES (5, "Gentille", 75);
+INSERT INTO second_table(id, name, score) VALUES (6, "Uwimbazi", 69);
