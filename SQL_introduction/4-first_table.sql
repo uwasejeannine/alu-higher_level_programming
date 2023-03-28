1fff443297dbf7f5658eb2504c79114b3c798301
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 02:04:43 -0700

    add

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,5 @@
 --a creapt that create a table called first table
 --The database name will be passed as an argument of the mysql
 CREATE TABLE IF NOT EXISTS first_table (
-  id INT,
-  name VARCHAR(256)
-);
+	id INT,
+	name VARCHAR(256));
