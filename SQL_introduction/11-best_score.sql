b3b6d7e89a792287fe078e5880e146a8643e3443
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:38:02 -0700

    change the code

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- A script that lists all records with a score >= 10 in the table second_table
 -- The database name will be passed as an argument of the mysql command
 SELECT score, name FROM second_table
+WHERE score >= 10
 ORDER BY score DESC;
