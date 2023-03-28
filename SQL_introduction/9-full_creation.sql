c8752395518cba47c75978a21b00d90ac1f9e9de
jeannineuwase <j.uwase1@alustudent.com>
2023-03-28 03:22:19 -0700

    add the corect name

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,10 @@
 -- A script that creates a table second_table in the database
 -- Add multiples rows.
-CREATE TABLE IF NOT EXISTS second_table(
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT);
-INSERT INTO second_table (id, name, score) VALUES (1, 'Jeannine', 78);
-INSERT INTO second_table (id, name, score) VALUES (2, 'Gentille', 67);
-INSERT INTO second_table (id, name, score) VALUES (3, 'Germen', 62);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
