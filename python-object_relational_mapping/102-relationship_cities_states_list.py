c93981c65875b1b2752bdc76556c5d043b84e296
jeannineuwase <j.uwase1@alustudent.com>
2023-04-24 04:27:08 -0700

    Python - Object-relational mapping

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
 """
-This script prints all City objects
-from the database hbtn_0e_14_usa.
+lists all City objects from the database hbtn_0e_101_usa
 """
-
-from sys import argv
-from model_state import State, Base
-from model_city import City
+import sys
+from relationship_state import Base, State
+from relationship_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-if __name__ == "__main__":
-    """
-    Access to the database and get the cities
-    from the database.
-    """
 
-    db_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3])
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
 
-    engine = create_engine(db_url)
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    results = session.query(City, State).join(State)
+    states = session.query(State).join(City).order_by(City.id).all()
 
-    for city, state in results.all():
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
-
-    session.commit()
+    for state in states:
+        for city in state.cities:
+            print("{}: {} -> {}".format(city.id, city.name, state.name))
