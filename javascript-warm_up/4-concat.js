3e2bda9c9f4d0342b10585030bb0e45b4ad4c01e
jeannineuwase <j.uwase1@alustudent.com>
2023-06-07 02:38:11 -0700

    remove double quate

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-const numArgs = args.length;
-
-if (numArgs >= 2) {
-  console.log(args[0] + ' is ' + args[1]);
-} else {
-  console.log('HBTN is undefined');
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
