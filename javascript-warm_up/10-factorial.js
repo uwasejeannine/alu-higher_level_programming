3e9930c11ab2386e1baa753110446cbd13c4194d
jeannineuwase <j.uwase1@alustudent.com>
2023-06-07 03:03:59 -0700

    add a file

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -3,6 +3,6 @@ function factorial (number) {
   if (isNaN(number) || number <= 1) {
     return 1;
   }
-  return number* factorial(number - 1);
+  return number * factorial(number - 1);
 }
 console.log(factorial(parseInt(process.argv[2])));
