e59b0a7013fbb4c0746f362516a20efb3bd90272
jeannineuwase <j.uwase1@alustudent.com>
2023-06-07 02:34:19 -0700

    remove double quate

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const numArgs = args.length;
+
+if (numArgs >= 2) {
+  console.log(args[0] + ' is ' + args[1]);
+} else {
+  console.log('You should provide two arguments.');
+}
