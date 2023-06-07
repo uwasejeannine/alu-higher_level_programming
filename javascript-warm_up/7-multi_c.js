eb45079115e5d1b97d61ba9219226956c55d1c6f
jeannineuwase <j.uwase1@alustudent.com>
2023-06-07 02:49:17 -0700

    remove double quate

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const number= parseInt(process.argv[2]);
+const number = parseInt(process.argv[2]);
 if (number) {
   for (let i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
